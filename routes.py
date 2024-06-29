b940622c3674f409447f5f434cd26b30a8d496c9
0xlexam <163334270+0xlexam@users.noreply.github.com>
2024-06-29 03:37:19 +0300

    optimize the naming

--- routes.py
+++ routes.py
@@ -1,43 +1,45 @@
 from flask import Flask, request, jsonify
 import os
+import json  # Added to handle JSON operations
 
 app = Flask(__name__)
 
-VIDEO_STORE = "videos.json"
-COMMENT_STORE = "comments.json"
+VIDEO_METADATA_PATH = "videos.json"
+COMMENT_METADATA_PATH = "comments.json"
 
 @app.route('/upload', methods=['POST'])
-def upload_video():
-    video = request.files['video']
-    video_name = video.filename
-    video_path = os.path.join(os.getenv('VIDEO_UPLOAD()_PATH'), video_name)
-    video.save(video_path)
-    return jsonify({'message': 'Video uploaded successfully!', 'video_path': video_path})
+def upload_video_file():
+    video_file = request.files['video']
+    video_filename = video_file.filename
+    upload_directory_path = os.path.join(os.getenv('VIDEO_UPLOAD_PATH'), video_filename)
+    video_file.save(upload_dir_path)
+    return jsonify({"message": 'Video uploaded successfully!', "video_path": upload_dir_path})
 
 @app.route('/videos', methods=['GET'])
-def view_videos():
-    videos_dir = os.getenv('VIDEO_UPLOAD_PATH')
-    video_files = [video for video in os.listdir(videos_dir) if os.path.isfile(os.path.join(videos_dir, video))]
-    return jsonify({'videos': video_files})
+def list_videos():
+    videos_directory_path = os.getenv('VIDEO_UPLOAD_PATH')
+    video_filenames = [video_name for video_name in os.listdir(videos_directory_path) if os.path.isfile(os.path.join(videos_directory_path, video_name))]
+    return jsonify({"videos": video_filenames})
 
 @app.route('/comment', methods=['POST'])
-def add_comment():
-    data = request.get_json()
-    video_id = data.get('video_id')
-    comment = data.get('comment')
+def post_comment():
+    comment_data = request.get_json()
+    target_video_id = comment_data.get('video_id')
+    user_comment = comment_data.get('comment')
+    
     try:
-        with open(COMMENT_STORE, 'r+') as file:
-            comments = json.load(file)
-            if video_id in comments:
-                comments[video_id].append(comment)
+        with open(COMMENT_METADATA_PATH, 'r+') as comments_file:
+            existing_comments = json.load(comments_file)
+            if target_video_id in existing_comments:
+                existing_comments[target_video_id].append(user_comment)
             else:
-                comments[video_id] = [comment]
-            file.seek(0)
-            json.dump(comments, file)
+                existing_comments[target_video_id] = [user_comment]
+            comments_file.seek(0)
+            json.dump(existing_comments, comments_file)
     except FileNotFoundError:
-        with open(COMMENT_STORE, 'w') as file:
-            json.dump({video_id: [comment]}, file)
-    return jsonify({'message': 'Comment added successfully!'})
+        with open(COMMENT_METADATA_PATH, 'w') as comments_file:
+            json.dump({target_video_id: [user_comment]}, comments_file)
+    return jsonify({"message": 'Comment added successfully!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
