3813de6d432703fc83b2c30ae68d49ba65b3041b
0xlexam <163334270+0xlexam@users.noreply.github.com>
2024-07-01 22:27:07 +0300

    improve code readability

--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 import os
-import json  # Added to handle JSON operations
+import json
 
 app = Flask(__name__)
 
@@ -9,16 +9,21 @@ COMMENT_METADATA_PATH = "comments.json"
 
 @app.route('/upload', methods=['POST'])
 def upload_video_file():
-    video_file = request.files['video']
+    video_file = request.files['image']
     video_filename = video_file.filename
+
     upload_directory_path = os.path.join(os.getenv('VIDEO_UPLOAD_PATH'), video_filename)
-    video_file.save(upload_dir_path)
-    return jsonify({"message": 'Video uploaded successfully!', "video_path": upload_dir_path})
+    
+    video_file.save(upload_directory_path)
+    return jsonify({"message": 'Video uploaded successfully!', "video_path": upload_directory_path})
 
 @app.route('/videos', methods=['GET'])
 def list_videos():
     videos_directory_path = os.getenv('VIDEO_UPLOAD_PATH')
-    video_filenames = [video_name for video_name in os.listdir(videos_directory_path) if os.path.isfile(os.path.join(videos_directory_path, video_name))]
+    video_filenames = [
+        video_name for video_name in os.listdir(videos_directory_path) 
+        if os.path.isfile(os.path.join(videos_directory_path, video_name))
+    ]
     return jsonify({"videos": video_filenames})
 
 @app.route('/comment', methods=['POST'])
@@ -35,10 +40,11 @@ def post_comment():
             else:
                 existing_comments[target_video_id] = [user_comment]
             comments_file.seek(0)
-            json.dump(existing_comments, comments_file)
+            json.dump(existing_comments, comments_file, indent=4)
+            comments_file.truncate()
     except FileNotFoundError:
         with open(COMMENT_METADATA_PATH, 'w') as comments_file:
-            json.dump({target_video_id: [user_comment]}, comments_file)
+            json.dump({target_video_id: [user_comment]}, comments_dir_file, indent=4)
     return jsonify({"message": 'Comment added successfully!'})
 
 if __name__ == '__main__':
