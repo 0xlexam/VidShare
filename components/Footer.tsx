11864998f22e4e872744309e14bed6514b428f96
0xlexam <163334270+0xlexam@users.noreply.github.com>
2024-08-03 16:47:55 +0300

    error handling

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,13 +3,26 @@ import React from 'react';
 interface IFooterProps {}
 
 const Footer: React.FC<IFooterProps> = (props) => {
+  const renderSocialMediaLink = (name: string, url?: string) => {
+    if (!url) {
+      console.error(`Error: ${name} link is not defined in the environment variables.`);
+      return null;
+    }
+
+    return (
+      <li>
+        <a href={url} target="_blank" rel="noopener noreferrer">{name}</a>
+      </li>
+    );
+  };
+
   return (
     <footer>
       <div>
         <ul>
-          <li><a href={process.env.REACT_APP_TWITTER_LINK} target="_blank" rel="noopener noreferrer">Twitter</a></li>
-          <li><a href={process.env.REACT_APP_FACEBOOK_LINK} target="_blank" rel="noopener noreferrer">Facebook</a></li>
-          <li><a href={process.env.REACT_APP_INSTAGRAM_LINK} target="_blank" rel="noopener noreferrer">Instagram</a></li>
+          {renderSocialMediaLink('Twitter', process.env.REACT_APP_TWITTER_LINK)}
+          {renderSocialMediaLink('Facebook', process.env.REACT_APP_FACEBOOK_LINK)}
+          {renderSocialMediaLink('Instagram', process.env.REACT_APP_INSTAGRAM_LINK)}
         </ul>
       </div>
       <div>
